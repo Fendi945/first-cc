6a2029a6434aefe68ecf03a175fe67bf6487fe72
Fendi945 <416811783@qq.com>
2026-08-26 23:45:39 +0800

    deploy: 大一造园官网精简部署(仅官网文件,移除仓库泄漏)

--- 启动引擎.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM 元演引擎启动脚本（解决 Windows GBK 编码问题）
-cd /d "%~dp0"
-set PYTHONIOENCODING=utf-8
-echo 正在启动元演引擎...
-start "元演引擎" python -m engine.main
-echo 引擎已启动，请稍候...
-echo 审批面板: http://127.0.0.1:8765/dashboard/
